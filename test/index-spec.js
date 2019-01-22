52f67078bb764c39aaa10de5fd01c3cc5e1ce759
shooes <shoo.es270@gmail.com>
2019-01-22 15:23:51 +0900

    [#12] Make test cases to fit given-when-then structure using RxJS
    Co-authored-by: jadenjack <jij5379@naver.com>

--- test/index-spec.js
+++ test/index-spec.js
@@ -2,14 +2,16 @@ import * as mocha from 'mocha';
 import chai from 'chai';
 import io from 'socket.io-client';
 
+import * as Rx from 'rxjs-compat';
 import Server from '../src/server';
 
 const { describe, it } = mocha;
 const { assert } = chai;
-const url = 'ws://localhost:3000';
 
 describe('Connection Test', () => {
+  const url = 'ws://localhost:3000';
   const server = new Server();
+
   beforeEach((done) => {
     server.start();
     done();
@@ -23,36 +25,47 @@ describe('Connection Test', () => {
   it('should echo hello', (done) => {
     // given
     const client = io.connect(url, null);
+    const observable = Rx.Observable
+      .fromEvent(client, 'echo')
+      .first();
+
     // when
-    client.once('connect', () => {
-      client.once('echo', (data) => {
-        // then
-        assert.equal(data, 'hello');
+    client.emit('echo', 'hello');
+
+    // then
+    observable.subscribe((data) => {
+      assert.equal(data, 'hello');
+    },
+    (err) => {
+      assert.fail(err);
+    })
+      .add(() => {
         client.disconnect();
         done();
       });
-      client.emit('echo', 'hello');
-    }).on('connect_error', (error) => {
-      // if connection is invalid,
-      // test should fail
-      assert.fail(error);
-      client.disconnect();
-      done();
-    });
   });
 
   it('A user should be responded "created" to "dial"', (done) => {
     // given
     const client = io.connect(url, null);
+    const observable = Rx.Observable
+      .fromEvent(client, 'created')
+      .first();
+
     // when
-    client.once('connect', () => {
-      client.once('created', (data) => {
-        // then
+    client.emit('dial');
+
+    // then
+    observable
+      .subscribe((data) => {
         assert.equal(data, 'created success');
+      },
+      (err) => {
+        assert.fail(err);
+      })
+      .add(() => {
         client.disconnect();
         done();
       });
-      client.emit('dial');
-    });
   });
 });
