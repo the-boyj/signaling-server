2a5055ab6d0fc41230f2fd6ff353a4db879e74b8
오석현 <shoo.es270@gmail.com>
2019-01-28 19:31:43 +0800

    Merge from Issue#37 to master (#40)
    * Change feature version based on 1.2.0
    
    * Add io object to event handlers
    
    * Change version to v1.2.0 before merging

--- test/events/webrtc-spec.js
+++ test/events/webrtc-spec.js
@@ -5,72 +5,70 @@ import * as webrtc from '../../src/events/webrtc';
 const { describe, it } = mocha;
 const { assert } = chai;
 
-describe('webrtc tests', () => {
-  describe('Sice Test', () => {
-    describe('Sice Error Test', () => {
-      it('should emit "peer_error" when candidate is invalid', (done) => {
-        // given
-        const receiver = {
-          messageBox: [],
-          emit: (eventName, message) => {
-            const msg = { eventName, message };
-            receiver.messageBox.push(msg);
-          },
-        };
-        const invalids = [
-          undefined,
-          null,
-          {},
-          { deviceToken: '' },
-          { deviceToken: null },
-          { deviceToken: undefined },
-          { sdpMid: 'sdpMid' }, // deviceToken undefined
-        ];
-        const eventName = 'peer_error';
-
-        invalids.forEach((candidate, index) => {
-          // when
-          webrtc.sice(receiver)(candidate);
-          // then
-          assert.equal(receiver.messageBox.length, index + 1);
-          assert.equal(receiver.messageBox[index].eventName, eventName);
-          assert.equal(receiver.messageBox[index].message, candidate);
-        });
-        done();
-      });
-    });
-    describe('Sice Test', () => {
+describe('Sice Test', () => {
+  describe('Sice Error Test', () => {
+    it('should emit "peer_error" when candidate is invalid', (done) => {
       // given
-      const candidate = {
-        deviceToken: '12345',
-        sdpMid: 'IamSDPMid',
-        sdpMLineIndex: '12345',
-        candidate: 'candidate:1234567890 1 udp 0987654321 192.168.0.1',
-      };
       const receiver = {
-        deviceToken: null,
-        messageBox: {},
-        to: (deviceToken) => {
-          receiver.deviceToken = deviceToken;
-          return receiver;
-        },
+        messageBox: [],
         emit: (eventName, message) => {
           const msg = { eventName, message };
-          const token = receiver.deviceToken;
-          if (!receiver.messageBox[token]) {
-            receiver.messageBox[token] = [];
-          }
-          receiver.messageBox[token].push(msg);
+          receiver.messageBox.push(msg);
         },
       };
-      const eventName = 'rice';
-      // when
-      webrtc.sice(receiver)(candidate);
-      // then
-      const { deviceToken } = receiver;
-      assert.equal(receiver.messageBox[deviceToken].length, 1);
-      assert.equal(receiver.messageBox[deviceToken][0].eventName, eventName);
-      assert.deepEqual(receiver.messageBox[deviceToken][0].message, candidate);
+      const invalids = [
+        undefined,
+        null,
+        {},
+        { deviceToken: '' },
+        { deviceToken: null },
+        { deviceToken: undefined },
+        { sdpMid: 'sdpMid' }, // deviceToken undefined
+      ];
+      const eventName = 'peer_error';
+
+      invalids.forEach((candidate, index) => {
+        // when
+        webrtc.sice()(receiver)(candidate);
+        // then
+        assert.equal(receiver.messageBox.length, index + 1);
+        assert.equal(receiver.messageBox[index].eventName, eventName);
+        assert.equal(receiver.messageBox[index].message, candidate);
+      });
+      done();
     });
+  });
+  describe('Sice Test', () => {
+    // given
+    const candidate = {
+      deviceToken: '12345',
+      sdpMid: 'IamSDPMid',
+      sdpMLineIndex: '12345',
+      candidate: 'candidate:1234567890 1 udp 0987654321 192.168.0.1',
+    };
+    const receiver = {
+      deviceToken: null,
+      messageBox: {},
+      to: (deviceToken) => {
+        receiver.deviceToken = deviceToken;
+        return receiver;
+      },
+      emit: (eventName, message) => {
+        const msg = { eventName, message };
+        const token = receiver.deviceToken;
+        if (!receiver.messageBox[token]) {
+          receiver.messageBox[token] = [];
+        }
+        receiver.messageBox[token].push(msg);
+      },
+    };
+    const eventName = 'rice';
+    // when
+    webrtc.sice()(receiver)(candidate);
+    // then
+    const { deviceToken } = receiver;
+    assert.equal(receiver.messageBox[deviceToken].length, 1);
+    assert.equal(receiver.messageBox[deviceToken][0].eventName, eventName);
+    assert.deepEqual(receiver.messageBox[deviceToken][0].message, candidate);
   });
 });
